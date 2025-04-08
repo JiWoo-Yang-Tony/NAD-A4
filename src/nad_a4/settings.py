5480e3362c689cc1dce105d7e95695d214789396
Jiwoo-Tony-Yang <gksmf1789@gmail.com>
2025-04-08 16:48:04 -0400

    chapter 3 - 15 Adding a Form to the Modal

--- src/nad_a4/settings.py
+++ src/nad_a4/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'profiles',
     # 3rd party apps
     'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy Forms settings
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
