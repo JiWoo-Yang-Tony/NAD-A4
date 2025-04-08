d8e0c4909dd619d7e1ba0b496e3dd75e676132a1
Jiwoo-Tony-Yang <gksmf1789@gmail.com>
2025-04-08 11:24:48 -0400

    chapter 3 - 13 Like Button with Ajax - Part 2: Something is not working!!!

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -5,6 +5,47 @@ const spinnerBox = document.getElementById('spinner-box')
 const loadBtn = document.getElementById('load-btn')
 const endBox = document.getElementById('end-box')
 
+const getCookie =(name)  => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+const likeUnlikePosts = () => {
+    const likeUnlikeForms = [...document.getElementsByClassName('like-unlike-forms')]
+    likeUnlikeForms.forEach(form=> form.addEventListener('submit', e=> {
+        e.preventDefault()
+        const clickedId = e.target.getAttribute('data-form-id')
+        const clickedBtn = document.getElementById(`like-unlike-btn-${clickedId}`)
+
+        $.ajax({
+            type: 'POST',
+            url: "/like-unlike/",
+            data: {
+                'csrfmiddlewaretoken': csrftoken,
+                'pk': clickedId,
+            },
+            success: function(response) {
+                console.log(response)
+                clickedBtn.textContent = response.liked ? `Unlike (${response.count})` : `Like (${response.count})`
+            },
+            error: function(error) {
+                console.log(error)
+            }
+        })
+    }))
+}
+
 let visible = 3
 
 const getData = () => {
@@ -15,7 +56,7 @@ const getData = () => {
             console.log(response)
             const data = response.data
             setTimeout(() => {
-                spinnerBox.classList.add('not-visisble')
+                spinnerBox.classList.add('not-visible')
                 console.log(data)
                 data.forEach(el => {
                     postsBox.innerHTML += `
@@ -30,19 +71,22 @@ const getData = () => {
                                         <a href="#" class="btn btn-primary">Details</a>
                                     </div>
                                     <div class="col-2">
-                                        <a href="#" class="btn btn-primary">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</a>
+                                        <form class="like-unlike-forms" data-form-id="${el.id}">
+                                            <button href="#" class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     `
                 });
+                likeUnlikePosts()
             }, 100)
             console.log(response.size)
             if (response.size === 0) {
                 endBox.textContent = 'No posts added yet...'
             } else if (response.size <= visible) {
-                loadBtn.classList.add('not-visisble')
+                loadBtn.classList.add('not-visible')
                 endBox.textContent = 'No more posts to load...'
             }
         },
@@ -53,7 +97,7 @@ const getData = () => {
 }
 
 loadBtn.addEventListener('click', () => {
-    spinnerBox.classList.remove('not-visisble')
+    spinnerBox.classList.remove('not-visible')
     visible += 3
     getData()
 })
